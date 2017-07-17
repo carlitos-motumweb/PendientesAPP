4760f10465827edc27cac7d2913ac987e0b53ea6
carlos augusto flores valerio <carlosflores@motumweb.com>
2017-07-17 00:55:18 -0500

    se agrega ventana de detalle

--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {Lista} from '../../app/clases/listas';
+import {ListaItem} from '../../app/clases/lista_item';
+import {ListaDeseosService} from '../../app/services/lista_deseos.service';
 
 @Component({
   selector: 'app-detalle',
@@ -8,11 +11,18 @@ import { NavController, NavParams } from 'ionic-angular';
 export class DetalleComponent implements OnInit {
     idx:number;
     lista:any;
-  constructor(private _navController: NavController, private _navParams: NavParams) {
+  constructor(private _navController: NavController,
+    private _navParams: NavParams,
+    private _listaDeseosServices: ListaDeseosService) {
     console.log(_navParams);
     this.idx = this._navParams.get('indice');
     this.lista = this._navParams.get('lista');
   }
 
   ngOnInit() { }
+
+  private marcarTarea(tarea: ListaItem){
+    tarea.completado = !tarea.completado;
+    this._listaDeseosServices.actualizarData();
+  }
 }
